e8c12204854ad8f89a35f375b95e3ccb6ff11550
IsaacValenciaZ <al222210520@gmail.com>
2026-03-09 06:14:33 -0600

    Subida de estatus

--- backend/config_mail.php
+++ backend/config_mail.php
@@ -1,7 +1,7 @@
 <?php
 
 define('MAIL_HOST', 'smtp.gmail.com');      
-define('MAIL_USER', 'depto.tecnicocorreo@seiem.gob.mx'); 
-define('MAIL_PASS', 'gfdc aplx  tesy hwqn'); 
+define('MAIL_USER', 'depto.tecnico@seiem.gob.mx'); 
+define('MAIL_PASS', 'gfdc aplx tesy hwqn'); 
 define('MAIL_PORT', 587);
 ?>
